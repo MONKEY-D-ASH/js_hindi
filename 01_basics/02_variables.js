d7e38046e555767247ca32cca3d219a15a197002
Aashish K Rana <136432799+MONKEY-D-ASH@users.noreply.github.com>
2025-07-24 14:45:18 +0000

    datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using node.js , not browser
+
+console.log(3+3)
+
+// code readability should be high
+// for documentation prefer either MDN or ECMAScript ecma script is the original one but it is harder to read and understand 
+
+console.log("aashish")
+
+let name= "aashish"
+let age = 18
+let is_logged_in = false
+let state = null
+
+// number => 2 to the power 53
+// bigint => this is used for very large numbers
+// string => prefer double quotes ""
+// boolean => true/false
+// null => stand_alone value
+// undefined => 
+// symbol => this is used for defining uniqueness 
+// object
+
+console.log(typeof undefined) // its output is undefined
+console.log(typeof null) // its output is object
