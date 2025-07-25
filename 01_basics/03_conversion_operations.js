b5b60ddd8ce7c81b6234ae637ea058a8a3968cfd
Aashish K Rana <136432799+MONKEY-D-ASH@users.noreply.github.com>
2025-07-25 14:31:20 +0000

    conversion and operations in js

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,27 @@
+let score = "aashish"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let value_in_number = Number(score)
+// console.log(typeof value_in_number);
+// console.log(value_in_number)
+
+// while converting a variable use the datatype with the first letter capital
+// "33" this string can easily be converted into a number
+// but "33abs" cannot be converted into a number as it gives a NaN output which stands for not a number
+
+let is_logged_in = "aashish"
+let boolean_is_logged_in = Boolean(is_logged_in)
+// console.log(boolean_is_logged_in)
+
+// "true" gives 1 and "false" gives 0
+//  "" empty string give false and "aashish" gives true\
+
+let sum_number = 69
+let sum_string = String(sum_number)
+console.log(sum_string)
+console.log(typeof sum_string)
+
+console.log(sum_string + 3);
+console.log(sum_number + 3);
