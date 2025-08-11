8036732958feab190b163671a7b5cb6eb6b1074b
Aashish K Rana <136432799+MONKEY-D-ASH@users.noreply.github.com>
2025-08-11 14:57:41 +0000

    date and time

---
+++ 01_basics/08_date_&_time.js
@@ -0,0 +1,16 @@
+// Dates
+
+let my_date = new Date(); // creating a new date object called my_date
+// console.log(my_date.toString());
+// console.log(my_date.toDateString());
+// console.log(my_date.toISOString());
+// console.log(my_date.toJSON());
+// console.log(my_date.toLocaleDateString()); // this provides only date 
+// console.log(my_date.toLocaleString()); // this provides both date and time 
+// console.log(my_date.toLocaleTimeString()); // this gives only time  
+
+console.log(typeof my_date);
+
+
+let myCreatedDate = new Date(2012, 0, 30)
+console.log(myCreatedDate);
