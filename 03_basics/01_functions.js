a7438cc99eaf7d8104811f1e997238f40dcc8d31
Aashish K Rana <136432799+MONKEY-D-ASH@users.noreply.github.com>
2025-08-18 14:35:52 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -48,6 +48,41 @@ function logInUserMessage(username = "ash"){   // you can also give a default va
 
 
 // console.log(logInUserMessage("aashish"));
-console.log(logInUserMessage()); // if you don't pass anything the value will be undefined
+// console.log(logInUserMessage()); // if you don't pass anything the value will be undefined
 
+
+function calculate_cart_price(...num1){  // these three dots is called rest operator, both spread and rest operator have the same syntax. what this will do is , it will pack all the argument and will return it in the form of an array.
+    return num1;
+}
+
+function calculate_cart_price2(val1, val2, ...num1){ // the first two arguments will be taken as val1 and val2 and the rest will be taken by the rest operator.
+    return num1;
+}
+
+console.log(calculate_cart_price(200,300,2500,343,244,2443));
+console.log(calculate_cart_price2(200,300,2500,343,244,2443));
+
+const user = {
+    username: "aashish",
+    price: 299
+}
+
+function handle_Object(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);   
+}
+
+// handle_Object(user) // either pass a predefined object.
+
+handle_Object({ // pass an object while calling the function.
+    username: "mayank",
+    price:349  
+})
+
+const my_new_array = [200,482,300,600]
+
+function return_second_value(arr){
+    return arr[1];
+}
+
+console.log(return_second_value(my_new_array));
 
