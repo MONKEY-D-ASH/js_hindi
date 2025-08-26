30d1fbecd930487e165ca7e95133cd54ac7d82fc
Aashish K Rana <136432799+MONKEY-D-ASH@users.noreply.github.com>
2025-08-26 15:18:40 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,45 @@
+// this is user to refer to the current context of a function or an object or anything in a scope 
+
+const user = {
+    username: "aashish",
+    price: 999,
+
+    welcome_message: function(){
+        console.log(`${this.username}, welcome to website`);   
+        console.log(this);
+    }
+}
+
+// user.welcome_message()
+// user.username = "sam"
+// user.welcome_message()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "tony"
+//     console.log(this.username); 
+// }
+
+// chai() // this throws undefined because this doesnt work inside a function 
+
+
+const chai = () => { // this is how you declare a function using an arrow function 
+    let username = "tony"
+    console.log(this.username);
+}
+
+// chai()
+
+// const add_two = (num1, num2) => {
+    // return num1 + num2
+// }
+
+// const add_two = (num1, num2) => num1 + num2  // this is an implicit return statement in which you don't have to use return statement 
+
+// const add_two = (num1, num2) => (num1 + num2) // this format is used very intensively in react.js, 
+const add_two = (num1, num2) => ({username: "aashish"}) // parenthesis are used in this format to return an object
+
+
+console.log(add_two(3,3));
+
